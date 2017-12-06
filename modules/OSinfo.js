aeda9bfdb4e62f3075396699ca8a3b9e5e3e5fc3
benice195 <szczyrbaw@gmail.com>
2017-12-07 00:28:55 +0100

    Zadanie 2 modul czasu

---
+++ modules/OSinfo.js
@@ -0,0 +1,25 @@
+var os = require('os');
+var time = require('../modules/TIMEinfo');
+
+function getOSinfo() {
+    var type = os.type();
+    
+    if (type === 'Darwin') {
+        type = 'OSX';
+    } else if (type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    
+    var userInfo = os.userInfo();
+    
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log(time.print());
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+}
+exports.print = getOSinfo;
