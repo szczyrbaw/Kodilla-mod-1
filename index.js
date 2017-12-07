e9647e29d78db034af402e52cae484e05e9c3e38
benice195 <szczyrbaw@gmail.com>
2017-12-07 16:45:19 +0100

    Zadanie 2 poprawki, Zadanie 3

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+var EventEmitter = require('events').EventEmitter;
 var OSinfo = require('./modules/OSinfo');
 var time = require('./modules/TIMEinfo');
 
-//OSinfo.print();
+var emitter = new EventEmitter();
+emitter.on("beforeCommand", function (instruction) {
+    console.log('You wrote: ' + instruction + ', trying to run command');
+});
+emitter.on("afterCommand", function () {
+    console.log('Finished command');
+});
+
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
+        emitter.emit('beforeCommand', instruction);
         switch (instruction) {
             case '/exit':
                 process.stdout.write('Quitting app!\n');
@@ -21,14 +30,15 @@ process.stdin.on('readable', function() {
                 process.stdout.write('Node version:\n');
                 process.stdout.write(process.versions.node);
                 break;
-            case '/p':
+            case '/getOSinfo':
                 OSinfo.print();
                 break;
             default: 
                 process.stdout.write('Avaible commands:\n');
                 process.stdout.write('/exit /lang /ver /getOSinfo \n');
                 break;
-        }
+        };
+        emitter.emit('afterCommand');
     }
 });
 
