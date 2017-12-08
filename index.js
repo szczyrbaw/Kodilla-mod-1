0d6f8f1bc0eab54fe7ba2a657f0e88287ad0506e
benice195 <szczyrbaw@gmail.com>
2017-12-08 14:26:52 +0100

    NodeJS File System

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 var EventEmitter = require('events').EventEmitter;
 var OSinfo = require('./modules/OSinfo');
 var time = require('./modules/TIMEinfo');
+var fs = require('fs');
+var StatMode = require('stat-mode');
+
+
+
+fs.readdir('./modules', 'utf-8', function(err, data) {
+    console.log(data);
+    fs.appendFile('./tekst.txt', '\nNazwy plików w folderze: ' + data , function(err) {
+        if (err) throw err;
+        console.log('Przeniesiono zawartość do tekst.exe'.red);
+        fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+            console.log('Dane po zapisie'.magenta);
+            console.log('\n' + data);
+        });
+    });
+});
+
+
+
+/*fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+    console.log('Dane przed zapisem'.blue);
+    console.log(data);
+    fs.appendFile('./tekst.txt', '\nA tak wygląda nowy tekst po zapisie!', function(err) {
+        if (err) throw err; //jesli pojawi się błąd zostanie wyrzucony wyjątek
+        console.log('zapisano!'.blue);
+        fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+            console.log('Całe dane po zapisie'.blue);
+            console.log(data);
+        });
+    });
+});*/
+
+
+
+/*fs.stat('./cat.jpg', function(err, stats) {
+    var statMode = new StatMode(stats);
+    console.log(statMode.toString());
+    // read and write permission
+})*/
+
 
 var emitter = new EventEmitter();
 emitter.on("beforeCommand", function (instruction) {
@@ -9,6 +49,10 @@ emitter.on("beforeCommand", function (instruction) {
 emitter.on("afterCommand", function () {
     console.log('Finished command');
 });
+
+
+
+
 
 
 process.stdin.setEncoding('utf-8');
